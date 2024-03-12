b835c256acd2edceea909aeecc43a29d20e12d23
Nithinreddy001 <118926532+Nithinreddy001@users.noreply.github.com>
2024-03-12 17:34:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_file
 from flask_cors import CORS  # Import CORS
 import cv2
 import numpy as np
 import face_recognition
 import csv
+import os
 
 app = Flask(__name__)
 CORS(app)  # Add this line to enable CORS for your Flask app
+
+# Define the directory where the images are stored
+IMAGE_DIRECTORY = r'C:\Users\nithi\Desktop\upwork\v1-bundle\images'
 
 # Load the CSV file and extract stored face encodings
 stored_encodings = []
@@ -18,13 +22,13 @@ with open('face_encodings.csv', mode='r') as file:
         face_encoding = np.array(row[1:], dtype=np.float64)
         stored_encodings.append((image_name, face_encoding))
 
-def compare_face_encodings(face_encodings, stored_encodings, tolerance=0.6):
+def compare_face_encodings(face_encodings, stored_encodings, tolerance=0.5):
     matched_images = set()
     for face_encoding in face_encodings:
         for stored_encoding in stored_encodings:
             stored_image_name, stored_face_encoding = stored_encoding
             distance = face_recognition.face_distance([stored_face_encoding], face_encoding)
-            if distance <= tolerance:
+            if distance < tolerance:
                 matched_images.add(stored_image_name)
     return matched_images
 
@@ -49,7 +53,20 @@ def upload_file():
 
         matched_images = compare_face_encodings(face_encodings, stored_encodings)
         
-        return jsonify({'message': 'File successfully uploaded', 'matched_images': list(matched_images)})
+        # Generate URLs for downloading the matched images
+        download_urls = [request.host_url + "images/" + image_name for image_name in matched_images]
+        
+        print("Download URLs:", download_urls)  # Debugging print
+        
+        return jsonify({'message': 'File successfully uploaded', 'matched_images': download_urls})
+
+@app.route('/images/<path:image_name>', methods=['GET'])
+def download_image(image_name):
+    image_path = os.path.join(IMAGE_DIRECTORY, image_name)
+    if os.path.exists(image_path):
+        return send_file(image_path, as_attachment=True)
+    else:
+        return jsonify({'error': 'Image not found'})
 
 if __name__ == '__main__':
     app.run(debug=True)
